bbbe5ce1cc148d85b05eda7aa2a8fe05d17e8ea3
mohammad maso <mohammadmasoudie@gmail.com>
2020-12-29 12:04:53 +0330

    unused vars

--- src/contents/index.js
+++ src/contents/index.js
@@ -2,10 +2,10 @@ import posts from './posts';
 
 import pages from './pages';
 import categories from './categories';
-import tags from './tags';
+//import tags from './tags';
 import users from './users';
 import profile from './profile';
-import settings from './settings';
+//import settings from './settings';
 import comments from './comments';
 import dashboard from './dashboard';
 import media from './media';
